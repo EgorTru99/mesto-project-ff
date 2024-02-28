79f5f08661ae0af2dd780e870ae2a307303f6dfe
Egor Trufanov <trufanov.egor.1999@yandex.ru>
2024-02-28 10:40:54 +0300

    Проведен рефакторинг функций

--- scripts/index.js
+++ scripts/index.js
@@ -5,24 +5,25 @@ const placesList = places.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 function createCard(nameValue, linkValue){
-  let cardTemplate = document.querySelector('#card-template').content;
-  let cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
   cardElement.querySelector('.card__title').textContent = nameValue;
   cardElement.querySelector('.card__image').src = linkValue;
+  cardElement.querySelector('.card__image').alt = nameValue;
 
-  placesList.append(cardElement);
+  return cardElement;
 };
 
 // @todo: Функция удаления карточки
 placesList.addEventListener('click', function (event) {
   if(event.target.classList.contains('card__delete-button')){
-    let cardDelete = event.target.closest('.card');
+    const cardDelete = event.target.closest('.card');
     cardDelete.remove();
   }
 });
 
 // @todo: Вывести карточки на страницу
 initialCards.forEach(function (initialCards){
-  createCard(initialCards.name, initialCards.link);
+  placesList.append(createCard(initialCards.name, initialCards.link));
 });
